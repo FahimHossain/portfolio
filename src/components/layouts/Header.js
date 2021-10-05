0d0900435f2d1f2f31e9a5dceac547bf6aa16f66
Fahim Hossain <zenothermic@gmail.com>
2021-10-06 00:52:03 +0600

    -

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './../css/main.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Navbar, Nav, Container, Row, Col} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
 export default function Header() {
@@ -18,6 +18,23 @@ export default function Header() {
                     </Col>
                 </Row>
             </Container>
+
+            {/* <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">Fahim Hossain</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="menu">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="/About">About</Nav.Link>
+                    <Nav.Link href="/Contact">Contact</Nav.Link>
+                    <Nav.Link href="#Photography">Link</Nav.Link>
+
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar> */}
         </div>
+        
     )
 }
